12336cc67eba5dfbf2078fdc267297f3031e73e1
WitsaKrm <witsarut.sx@gmail.com>
2023-08-26 18:02:29 +0700

    update node.api.jsx

--- frontend/src/services/API/node.api.jsx
+++ frontend/src/services/API/node.api.jsx
@@ -1,45 +1,54 @@
-// // node.api.jsx
-// import { useEffect } from "react";
-// import API from "../../services/API/axios";
 
+import API from "../../services/API/axios";
 
-// export function FetchNodes(setNodes) {
-//     const fetchNodes = async () => {
-//         try {
-//           const res = await API.get("/nodes");
-//           setNodes(res.data.nodes);
-//         } catch (error) {
-//           console.error("Failed to fetch nodes:", error);
-//         }
-//       };
-// }
-// const FetchNodes = (setNodes) => {
-//   useEffect(() => {
-//     const fetchNodes = async () => {
-//       try {
-//         const res = await API.get("/nodes");
-//         setNodes(res.data.nodes);
-//       } catch (error) {
-//         console.error("Failed to fetch nodes:", error);
-//       }
-//     };
-//     fetchNodes();
-//   }, [setNodes]);
-// };
+export function FetchNodes(SET,URL,ID) {
+    const fetchNodes = async () => {
+        try {
+          const res = await API.get(URL);
+          console.log(res.data.nodes);
+          SET(res.data.nodes);
+        } catch (error) {
+          console.error("Failed to fetch nodes:", error);
+        }
+      };
+      fetchNodes()
+}
 
-// const FetchStations = (setStations) => {
-//   useEffect(() => {
-//     const fetchStations = async () => {
-//       try {
-//         const res = await API.get("/station");
-//         setStations(res.data.stations);
-//       } catch (error) {
-//         console.error("Failed to fetch stations:", error);
-//       }
-//     };
-//     fetchStations();
-//   }, [setStations]);
-// };
+export function FetchStations(SET,URL,ID) {
+    const fetchStations = async () => {
+        try {
+          const res = await API.get(URL);
+          SET(res.data.stations);
+        } catch (error) {
+          console.error("Failed to fetch stations:", error);
+        }
+      }; 
+      fetchStations()  
+}
 
-
-// export { FetchNodes, FetchStations };
+export function FetchSensers (SET,URL,ID) {
+    const fetchSensers = async () => {
+    try {
+      const res = await API.get(`${URL}/${ID}`);
+      console.log(res.data);
+      SET(res.data.senser || []);
+    } catch (error) {
+      console.error("Failed to fetch senser:", error);
+    }
+  };
+  fetchSensers()
+}
+export function FetchChart (SET,URL,ID) {
+    const fetchChart = async () => {
+        console.log(URL);
+    try {
+      const res = await API.get(`${URL}/${ID}`);
+      console.log(`${URL}/${ID}`);
+      console.log(res.data);
+      SET(res.data.chart || []);
+    } catch (error) {
+      console.error("Failed to fetch senser:", error);
+    }
+  };
+  fetchChart()
+}
