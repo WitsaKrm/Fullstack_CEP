f1425f9910a41465e06d6c74a194ff099ea565c7
WitsaKrm <witsarut.sx@gmail.com>
2023-09-01 02:15:17 +0700

    edit to use devices table

--- frontend/src/pages/indexPage/indexPage.jsx
+++ frontend/src/pages/indexPage/indexPage.jsx
@@ -3,23 +3,20 @@ import { useHistory } from "react-router-dom";
 import NodeBox from "../../components/node/node";
 import "./indexPage.css";
 import AppHeader from "../../components/header/app-header";
-import { FetchNodes, FetchStations } from "../../services/API/node.api";
+import { FetchDevicesById } from "../../services/API/node.api";
 import svg from "../../assets/svg/svg"; // Import SVG icons properly based on your file structure
 
-const NODES_URL = `/nodes`;
-const STATION_URL = `/station`;
+const DEVICE_URL = `/devices`;
 
 const IndexPage = () => {
   const history = useHistory();
-  const [nodes, setNodes] = useState([]);
-  const [stations, setStations] = useState([]);
+  const [devices, setDevices] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     async function fetchData() {
       try {
-        await FetchNodes(setNodes,NODES_URL);
-        await FetchStations(setStations,STATION_URL);
+        await FetchDevicesById(setDevices, DEVICE_URL);
         setIsLoading(false); // Data fetching is complete, set isLoading to false
       } catch (error) {
         console.error("Error fetching data:", error);
@@ -34,64 +31,70 @@ const IndexPage = () => {
   };
 
   if (isLoading) {
-    return <p>Loading...</p>; // Show a loading message while fetching data
+    return <p>Loading...</p>;
   }
 
   return (
     <>
       <AppHeader nameHeader="ComputerEng PROJECT" />
-      <div className="container mt-4">
+      <div className="container">
         <div className="col">
           <div className="row">
-            {stations.map((station) => (
-              <div
-                className="node col-md-3 d-flex justify-content-center"
-                key={station.station_id}
-              >
-                <NodeBox
-                  name={station.s_name.toUpperCase()}
-                  src={
-                    station.type === "station"
-                      ? svg.station.default
-                      : svg.node.default
-                  }
-                  status={station.status.toString()}
-                  handleNodeClick={() =>
-                    handleNodeClick(
-                      station.userId,
-                      station.type === "station" ? "station" : "senser",
-                      station.station_id
-                    )
-                  }
-                />
-              </div>
-            ))}
+            {devices.map(
+              (station) =>
+                // Check if station.type is "station" before rendering the NodeBox component
+                station.type === "station" && (
+                  <div
+                    className="node col-md-3 d-flex"
+                    key={station.d_id}
+                  >
+                    <NodeBox
+                      name={station.d_name.toUpperCase()}
+                      src={
+                        station.type === "station"
+                          ? svg.station.default
+                          : svg.station.default
+                      }
+                      status={station.status.toString()}
+                      handleNodeClick={() =>
+                        handleNodeClick(
+                          station.userId,
+                          "station",
+                          station.station_id
+                        )
+                      }
+                    />
+                  </div>
+                )
+            )}
           </div>
-
           <div className="row">
-            {nodes.map((node) => (
-              <div
-                className="node col-md-3 d-flex justify-content-center"
-                key={node.node_id}
-              >
-                <NodeBox
-                  name={node.n_name.toUpperCase()}
-                  src={
-                    node.type === "station"
-                      ? svg.station.default
-                      : svg.node.default
-                  }
-                  status={node.status.toString()}
-                  handleNodeClick={() =>
-                    handleNodeClick(
-                      node.userId,
-                      node.type === "station" ? "station" : "senser",
-                      node.node_id
-                    )
-                  }
-                />
-              </div>
-            ))}
+            {devices.map(
+              (node) =>
+                node.type === "node" && (
+                  <div
+                    className="node col-md-3 d-flex"
+                    key={node.d_id}
+                  >
+                    <NodeBox
+                      name={node.d_name.toUpperCase()}
+                      src={
+                        node.type === "node"
+                          ? svg.node.default
+                          : svg.station.default
+                      }
+                      status={node.status.toString()}
+                      handleNodeClick={() =>
+                        handleNodeClick(
+                          node.userId,
+                          node.type === "station" ? "station" : "senser",
+                          node.node_id
+                        )
+                      }
+                    />
+                  </div>
+                )
+            )}
           </div>
         </div>
       </div>
