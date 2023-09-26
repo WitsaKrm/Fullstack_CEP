5391251f5d9666a1fb00bf0808f5c3642760a003
WitsaKrm <witsarut.sx@gmail.com>
2023-09-27 04:17:22 +0700

    other --

--- frontend/src/assets/svg/svg.jsx
+++ frontend/src/assets/svg/svg.jsx
@@ -4,7 +4,8 @@ const svg = {
     soil_mois: require("./soil-mois-SVG.svg"),
     light: require("./light-SVG.svg"),
     station: require('./stations-svg.svg'),
-    node: require('./sensers-svg.svg')
+    node: require('./sensers-svg.svg'),
+    waterLevel: require('./waterlevel-PNG.png')
   };
   
   export default svg;
