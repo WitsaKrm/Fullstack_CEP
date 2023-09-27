5e5a525c5f0f56d74d611aa28509337865b54c87
WitsaKrm <witsarut.sx@gmail.com>
2023-09-28 01:03:10 +0700

    set handle set new data

--- frontend/src/pages/users/StationPage/stationPage.jsx
+++ frontend/src/pages/users/StationPage/stationPage.jsx
@@ -1,38 +1,78 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useHistory } from "react-router-dom";
 import AppHeader from "../../../components/header/app-header";
 import style from "./stationPage.module.css";
-
 import Box from "../../../components/box/box";
 import { useParams, useLocation } from "react-router-dom";
 import WaterLevel from "../../../components/waterlevel/waterlevel";
 import LevelSlide from "../../../components/waterlevel/levelslide";
 import Maps from "../../../components/maps/map";
+import { FetchMode, SetModeNewStation } from "../../../services/API/mode.api";
+import endpoint from "../../../services/API/axios";
+
+const MODE_URL = "/mode";
 
 const StationPage = () => {
   const { nodeId } = useParams();
   const history = useHistory();
-  const [mode, setMode] = useState();
-  const [isAutoVisible, setIsAutoVisible] = useState();
-  const [isManualVisible, setIsManualVisible] = useState();
+  const [modeData, setModeData] = useState([]);
+  const [isAutoVisible, setIsAutoVisible] = useState(false);
+  const [isManualVisible, setIsManualVisible] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const [succ, setSucc] = useState(false);
+  const [error, setError] = useState("");
 
-  // Use useLocation to access query parameters
   const location = useLocation();
   const queryParams = new URLSearchParams(location.search);
   const lat = queryParams.get("lat");
   const lon = queryParams.get("lon");
 
-  const handleAutoClick = () => {
-    setIsAutoVisible(true);
-    setIsManualVisible(false);
-  };
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        await SetModeNewStation(setModeData, MODE_URL, `${nodeId}`);
+        await FetchMode(setModeData, MODE_URL, `${nodeId}`); // Await this promise
+        setIsLoading(false);
+        // Update isAutoVisible and isManualVisible based on data
+        // if (data.length > 0 && data[0].mode === "MANUAL") {
+        //   setIsManualVisible(true);
+        //   setIsAutoVisible(false);
+        // } else {
+        //   setIsAutoVisible(true);
+        //   setIsManualVisible(false);
+        // }
+      } catch (error) {
+        console.error("Error fetching data:", error);
+        setIsLoading(false);
+      }
+    }
+    fetchData();
+  }, [nodeId]);
+  
 
-  const handleManualClick = () => {
-    setIsAutoVisible(false);
-    setIsManualVisible(true);
-  };
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
 
-  // Define HTML elements as constants
+  const handleSlide = async (dataGroup) => {
+    try {
+      const response = await endpoint.put(
+        `${MODE_URL}/${modeData.devices_node_id}`,
+        dataGroup
+      );
+      console.log(response);
+      if (response.status === 200) {
+        setSucc(true);
+        setTimeout(() => {
+          window.location.reload();
+        }, 500);
+      } else {
+        setError("Error updating Mode: " + response.statusText);
+      }
+    } catch (error) {
+      setError("Error updating Mode: " + error.message);
+    }
+  };
   const autoContent = () => (
     <>
       <h1>AUTO mode</h1>
@@ -67,51 +107,57 @@ const StationPage = () => {
       </div>
     </>
   );
-
-  const manualContent = () => (
+  const manualContent = (modeData) => (
     <>
       <h1>MANUAL</h1>
-      <Box
-        detail={
-          <>
-            <WaterLevel
-              waterLevel
-              maxWaterHeight={300}
-              date={"9/9/2023"}
-            ></WaterLevel>
-            <LevelSlide
-              level={0}
-              // handleSlide={handleSlide}
-            ></LevelSlide>
-          </>
-        }
-      ></Box>
+      <div className={style.box_manual}>
+        <WaterLevel data={modeData} maxWaterHeight={300}></WaterLevel>
+        <LevelSlide data={modeData} handleSlide={handleSlide}></LevelSlide>
+      </div>
     </>
   );
-  return (
+
+  const handleAutoClick = () => {
+    setIsAutoVisible(true);
+    setIsManualVisible(false);
+  };
+  const handleManualClick = () => {
+    setIsAutoVisible(false);
+    setIsManualVisible(true);
+  };
+  console.log(modeData);
+return (
     <>
       <AppHeader nameHeader="STATION" />
-      <Maps lat={lat} lon={lon} title={`STATION ${nodeId}`} detail={""}></Maps>
+      <Maps
+        lat={lat}
+        lon={lon}
+        title={`STATION ${nodeId}`}
+        detail={`ตำแหน่งที่ตั้ง ละติจูดที่${lat}, ลองติจูดที่ ${lon}`}
+      ></Maps>
       <div className="container">
         <div className={style.btt}>
-          <div
+          <button
             className={`${style.btn} btn btn-dark`}
             onClick={handleAutoClick}
+            disabled={modeData.mode === 'MANUAL'}
           >
             auto
-          </div>
-          <div
+          </button>
+          <button
             className={`${style.btn} btn btn-warning`}
             onClick={handleManualClick}
+            disabled={modeData.mode === 'AUTO'}
           >
             manual
-          </div>
+          </button>
         </div>
         <div className="control">
-          {(isManualVisible || isAutoVisible) && (
-            <div className={isAutoVisible ? style.auto : style.manual}>
-              {isAutoVisible ? autoContent() : manualContent()}
-            </div>
+          {isManualVisible&& (
+            <div className={style.manual}>{manualContent(modeData)}</div>
+          )}
+          {isAutoVisible && (
+            <div className={style.auto}>{autoContent()}</div>
           )}
         </div>
       </div>
@@ -120,3 +166,5 @@ const StationPage = () => {
 };
 
 export default StationPage;
+
+
