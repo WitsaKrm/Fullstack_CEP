f07b38a8aaf16c6c6d011853919ada06da7b1b46
WitsaKrm <witsarut.sx@gmail.com>
2023-10-04 02:14:48 +0700

    edit app.jsx

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,16 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Routes from "./services/Routes/routes.service";
-import { Helmet } from "react-helmet";
-import IndexPage from "./pages/users/indexPage/indexPage";
-import LoginPage from "./pages/LoginPage/loginPage";
-import SensersPage from "./pages/users/SenserPage/senserPage";
-import UsersPage from "./pages/admin/UsersPage/users";
-
-import AddUsers from "./components/addusers/addusers";
+import { Helmet, HelmetProvider } from 'react-helmet-async';
+// import IndexPage from "./pages/users/indexPage/indexPage";
+// import LoginPage from "./pages/LoginPage/loginPage";
+// import SensersPage from "./pages/users/SenserPage/senserPage";
+// import UsersPage from "./components/UsersTable/users";
+// import AddUsers from "./components/addusers/addusers";
 
 function App() {
   return (
+    <HelmetProvider>
     <div className="container">
       <Helmet>
         <link
@@ -21,9 +21,8 @@ function App() {
       <Router>
         <Routes />
       </Router>
-      {/* <UsersPage/>
-      <AddUsers /> */}
     </div>
+    </HelmetProvider>
   );
 }
 
