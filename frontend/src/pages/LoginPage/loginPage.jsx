1da3a502ef4cd97fe3fcfc32ed408a5fab369f2b
WitsaKrm <witsarut.sx@gmail.com>
2023-10-04 02:17:07 +0700

    update loginpage

--- frontend/src/pages/LoginPage/loginPage.jsx
+++ frontend/src/pages/LoginPage/loginPage.jsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
-import style from"./loginPage.module.css"
-import AppHeader from "../../components/header/app-header"
-import { useHistory } from 'react-router-dom';
-
+import style from "./loginPage.module.css";
+import AppHeader from "../../components/header/app-header";
+import { useHistory } from "react-router-dom";
+import Swal from "sweetalert2";
 import endpoint from "../../services/API/axios";
 import AddUsers from "../../components/addusers/addusers";
+import { SETUIDLocal } from "../../services/tkEndcoded.service";
 const LOGIN_URL = "/login";
+const USER = "/user";
 
 const LoginPage = () => {
   const history = useHistory();
@@ -22,54 +24,67 @@ const LoginPage = () => {
         password: userPwd,
       });
       console.log(endpoint, LOGIN_URL);
-      console.log("25",res.data);
+      console.log(res.data);
       if (res.data.status === "Success") {
-        console.log(res.data.text);
-        history.push({
-          pathname: "/",
+        const user = await endpoint.get(`${USER}/${userName}`);
+        console.log(user);
+        console.log(res.data.msg);
+        Swal.fire({
+          title: "กำลังเข้าสู่ระบบ",
+          icon: "success",
+          showConfirmButton: false,
+          timer: 500,
+        }).then(() => {
+          localStorage.setItem("token", res.data.token);
+          SETUIDLocal();
+          history.push({
+            pathname: `/`,
+          });
         });
       } else {
-        setErrMsg(res.data.text);
-        console.log("33",res.data.text);
-
+        setErrMsg(res.data.msg);
+        console.log(res.data.msg);
+        console.log(errMsg);
       }
     } catch (err) {
       console.error(err);
-      setErrMsg("No Server Response");
+      setErrMsg("Password incorrect : " + err.message);
     }
-    console.log(userName,userPwd);
+    console.log(userName, userPwd);
   };
 
   return (
     <>
-    <AppHeader/>
-    <section>
-      <h1>Login</h1>
-      <form onSubmit={handleLogin}>
-        <label htmlFor="username">Username : </label>
-        <input
-          type="text"
-          id="login-username"
-          value={userName}
-          onChange={(e) => setUserName(e.target.value)}
-          required
-        />
+      <AppHeader />
+      <section>
+        <h1>Login</h1>
+        <form onSubmit={handleLogin}>
+          <label htmlFor="username">Username : </label>
+          <input
+            type="text"
+            id="login-username"
+            value={userName}
+            onChange={(e) => setUserName(e.target.value)}
+            required
+          />
 
-        <label htmlFor="password">Password:</label>
-        <input
-          type="password"
-          id="login-password"
-          value={userPwd}
-          onChange={(e) => setUserPwd(e.target.value)}
-          required
-        />
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="login-password"
+            value={userPwd}
+            onChange={(e) => setUserPwd(e.target.value)}
+            required
+          />
 
-        {errMsg && <p className={style.errorMsg}>{errMsg}</p>}
+          {errMsg && <p className={style.errorMsg}>{errMsg}</p>}
 
-        <button className={style.loginBtt} type="submit">LOGIN</button>
-      </form>
-    </section>
-    <AddUsers/>
+          <button className={style.loginBtt} type="submit">
+            LOGIN
+          </button>
+        </form>
+      </section>
+      <AddUsers />
     </>
   );
 };
