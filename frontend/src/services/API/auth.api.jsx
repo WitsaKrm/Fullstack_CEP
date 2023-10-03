17aed1683af4374d5f9849ae95b320fb87e533c1
WitsaKrm <witsarut.sx@gmail.com>
2023-10-04 02:18:08 +0700

    create auth api

---
+++ frontend/src/services/API/auth.api.jsx
@@ -0,0 +1,45 @@
+import Swal from "sweetalert2";
+
+export async function authenticate(AUTH_URL, history) {
+    const token = localStorage.getItem("token");
+    try {
+      const response = await fetch(`http://localhost:9000${AUTH_URL}`, {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+      });
+  
+      if (response.status === 200) {
+        Swal.fire({
+          title: "ยืนยันตัวตนเสร็จสิ้น",
+          icon: "success",
+          showConfirmButton: false,
+          timer: 500,
+        });
+      } else {
+        // Authentication failed
+        localStorage.removeItem("token");
+        localStorage.removeItem("UID");
+        history.push({
+          pathname: "/login",
+        });
+      }
+    } catch (err) {
+      console.error("Authentication error:", err);
+      Swal.fire({
+        title: "Authentication Error",
+        text: "An error occurred during authentication.",
+        icon: "error",
+        showConfirmButton: false,
+        timer: 500,
+      }).then(() => {
+        localStorage.removeItem("token");
+        localStorage.removeItem("UID");
+        history.push({
+          pathname: "/login",
+        });
+      });
+    }
+  }
