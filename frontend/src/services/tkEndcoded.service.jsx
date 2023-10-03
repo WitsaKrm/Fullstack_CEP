c411f15c1cc8d6d1bf2dcf6c03839b6d0c7b1dd6
WitsaKrm <witsarut.sx@gmail.com>
2023-10-04 02:20:46 +0700

    tk - decode

---
+++ frontend/src/services/tkEndcoded.service.jsx
@@ -0,0 +1,32 @@
+// Import jwtDecode only once at the beginning of your file
+import jwtDecode from "jwt-decode";
+
+export function decodedToken(SET) {
+  const token = localStorage.getItem('token');
+  try {
+    const decodedToken = jwtDecode(token);
+    SET(decodedToken);
+  } catch (error) {
+    console.error("Error parsing the decoded token as JSON:", error);
+  }
+}
+
+export function SETUIDLocal() {
+  const token = localStorage.getItem('token');
+  if (!token) {
+    console.error("Token not found in localStorage");
+    return;
+  }
+  try {
+    const decodedToken = jwtDecode(token);
+    
+    if (decodedToken && decodedToken.user_id) {
+      localStorage.setItem('UID', decodedToken.user_id);
+    //   const UID = localStorage.getItem('UID');
+    } else {
+      console.error("Invalid decoded token:", decodedToken);
+    }
+  } catch (error) {
+    console.error("Error decoding token:", error);
+  }
+}
