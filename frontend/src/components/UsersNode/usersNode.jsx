353f663155190120ad1048aac8501a5a9cf63fb5
WitsaKrm <witsarut.sx@gmail.com>
2023-10-04 02:12:48 +0700

    craete userNodes conponent

---
+++ frontend/src/components/UsersNode/usersNode.jsx
@@ -0,0 +1,11 @@
+import style from './usersNode.module.css'
+
+const userNodes = (props) => {
+
+    return (
+        <>
+        <div className={style.container}></div>
+        </>
+    )
+}
+export default userNodes;
