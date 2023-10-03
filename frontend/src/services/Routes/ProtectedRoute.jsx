daeb505934d8c0a06a1e4c83533e6d5c03ab6759
WitsaKrm <witsarut.sx@gmail.com>
2023-10-04 02:19:31 +0700

    route Protected and route update

---
+++ frontend/src/services/Routes/ProtectedRoute.jsx
@@ -0,0 +1,23 @@
+// ProtectedRoute.js
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
+import { isAuthenticated ,getUserRole } from "../Auth.service"; // Import your authentication service
+
+const ProtectedRoute = ({ component: Component, isAdmin, ...rest }) => (
+    <Route
+      {...rest}
+      render={(props) => {
+        if (!isAuthenticated()) {
+          return <Redirect to="/login" />;
+        }
+        const userRole = getUserRole();
+        console.log(userRole);
+        if (isAdmin && userRole !== 0) {
+          return <Redirect to="/" />;
+        }
+        return <Component {...props} />;
+      }}
+    />
+  );
+  
+  export default ProtectedRoute;
--- frontend/src/services/Routes/routes.service.jsx
+++ frontend/src/services/Routes/routes.service.jsx
@@ -1,21 +1,57 @@
-import React from "react";
-import { Switch, Route } from "react-router-dom";
-import LoginPage from "../../pages/LoginPage/loginPage";
+// import React from "react";
+// import { Switch, Route } from "react-router-dom";
+// import LoginPage from "../../pages/LoginPage/loginPage";
 
+// import IndexPage from "../../pages/users/indexPage/indexPage";
+// import SensersPage from "../../pages/users/SenserPage/senserPage";
+// import StationPage from "../../pages/users/StationPage/stationPage";
+
+// import UsersPage from "../../pages/admin/UsersPage/users";
+
+// const Routes = () => {
+//   return (
+//     <Switch>
+//       <Route exact path="/" component={IndexPage} />
+//       <Route path="/users" component={UsersPage}/>
+//       <Route path="/login" component={LoginPage} />
+//       <Route path="/senser/:nodeId" component={SensersPage} />
+//       <Route path="/station/:nodeId" component={StationPage} />
+//     </Switch>
+//   );
+// };
+
+// export default Routes;
+import React from "react";
+import { Switch, Route, Redirect } from "react-router-dom";
+import { getUserRole } from "../Auth.service";
+import ProtectedRoute from "./ProtectedRoute";
+
+import AdminPage from "../../pages/admin/adminindex/adIndex";
+import LoginPage from "../../pages/LoginPage/loginPage";
 import IndexPage from "../../pages/users/indexPage/indexPage";
 import SensersPage from "../../pages/users/SenserPage/senserPage";
 import StationPage from "../../pages/users/StationPage/stationPage";
 
-import UsersPage from "../../pages/admin/UsersPage/users";
+import UsersPage from "../../components/UsersTable/users";
 
 const Routes = () => {
+  const userRole = getUserRole();
+  console.log("User Role:", userRole);
+  if(userRole === 0){
+console.log(userRole);
+  }
   return (
     <Switch>
-      <Route exact path="/" component={IndexPage} />
-      <Route path="/users" component={UsersPage}/>
+      <Route
+        exact
+        path="/"
+        component={() => (userRole === 0 ? <AdminPage/> : <IndexPage/>)}
+      />
+      <ProtectedRoute path="/users" component={UsersPage} isAdmin={true} />
       <Route path="/login" component={LoginPage} />
-      <Route path="/senser/:nodeId" component={SensersPage} />
-      <Route path="/station/:nodeId" component={StationPage} />
+      <ProtectedRoute path="/senser/:userId:nodeId" component={SensersPage} />
+      <ProtectedRoute path="/station/:userId:nodeId" component={StationPage} />
+      <Redirect to="/" /> {/* Redirect to the home page for unknown routes */}
     </Switch>
   );
 };
